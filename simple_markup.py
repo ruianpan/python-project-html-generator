2c6ab93f9c0aff28ce2b288a7a350dbef04b1212
unknown <ruianp2@illinois.edu>
2018-06-18 16:12:03 +0800

    book version code

--- simple_markup.py
+++ simple_markup.py
@@ -1,5 +1,7 @@
 import sys, re
 from util import *
+
+
 print('<html><head><title>...</title><body>')
 title = True
 for block in blocks(sys.stdin):
--- util.py
+++ util.py
@@ -1,13 +1,13 @@
 def lines(file):
     for line in file:
-        yield line
+        yield line   # normal ass line yield
     yield '\n'
 
 def blocks(file):
     block = []
     for line in lines(file):
-        if line.strip():
+        if line.strip():   #if we have anything in the line
             block.append(line)
-        elif block:
-            yield ''.join(block).strip()
+        elif block:         #if we have nothing in the line but something in the block
+            yield ''.join(block).strip()  #join the block into a string and yield
             block = []
